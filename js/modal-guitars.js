c2981948ddfe4c8e54073532ff0ebf174385263f
bondarenkoai <114067419+bondarenkoai@users.noreply.github.com>
2023-04-09 00:56:58 +0300

    mod-8.14

---
+++ js/modal-guitars.js
@@ -0,0 +1,31 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-login-open]"),
+        openModalBtn2: document.querySelector("[data-modal-login-open2]"),
+        closeModalBtn: document.querySelector("[data-modal-login-close]"),
+        modal: document.querySelector("[data-modal-login]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn2.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-signup-open]"),
+        closeModalBtn: document.querySelector("[data-modal-signup-close]"),
+        modal: document.querySelector("[data-modal-signup]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
